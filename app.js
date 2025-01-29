0fee7ad652450ddcb33acf2c48c75be6776cf9fe
ekajaya740 <ekajaya740@gmail.com>
2025-01-29 15:36:36 +0800

    feat: add /me route

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-'use strict'
+"use strict";
 
-const express = require('express')
+const express = require("express");
 
-const PORT = 8000
-const HOST = '0.0.0.0'
+const PORT = 8000;
+const HOST = "0.0.0.0";
 
-const app = express()
+const app = express();
 /*
 [ROUTE] '/'
 [Response] => Hello world!
  */
-app.get('/', (req, res) => {
-    res.send('Hello world!\n')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world!\n");
+});
+
+app.get("/me", (req, res) => {
+  res.send("ekajaya740\n");
+});
 
 /*
 Optional [ROUTE] '/me'
 [Response] => Dicoding username.
 */
 
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}`)
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
